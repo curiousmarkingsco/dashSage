bb359f18679cfe9599b587b5247226be486e1951
Ian McKenzie <admin@ianrandmckenzie.com>
2024-09-26 12:55:11 -0700

    Update appSage, clean up unused files, use on-brand icon, make 'Options' more inuititive

--- source/scripts/redirect.js
+++ source/scripts/redirect.js
@@ -7,4 +7,9 @@
     const newUrl = window.location.pathname + '?' + urlParams.toString() + window.location.hash;
     window.location.replace(newUrl);
   }
+  if (!urlParams.has('config')) {
+    urlParams.set('config', 'Dashboard');
+    const newUrl = window.location.pathname + '?' + urlParams.toString() + window.location.hash;
+    window.location.replace(newUrl);
+  }
 })();
