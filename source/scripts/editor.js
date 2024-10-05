f2c2cd4bf414e8538d0f671ffe4ad7d3700d65f8
Ian McKenzie <admin@ianrandmckenzie.com>
2024-10-05 14:30:03 -0700

    Make dS place nicle with latest aS updates

--- source/scripts/editor.js
+++ source/scripts/editor.js
@@ -10,3 +10,12 @@ const styleElement = document.getElementById('appSage-style');
 if (styleElement) {
   styleElement.href = chrome.runtime.getURL('appSage/dist/appSage.css');
 }
+
+// Hide font selection for now since CSP makes custom fonts a monolithic pain in the ass
+document.querySelectorAll('select').forEach(function(selectElement) {
+  const hasFontOption = Array.from(selectElement.options).some(option => option.value.startsWith('font-'));
+  
+  if (hasFontOption) {
+    selectElement.style.display = 'none';
+  }
+});
